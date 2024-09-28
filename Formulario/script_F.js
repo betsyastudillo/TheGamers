6f675d758479878c12ec3f2dc002f5e642b53180
IvanichSan <santiagol19@outlook.com>
2024-09-27 23:20:24 -0500

    Se agrega Captchap y se crea la funcion para ocular y mostrar

--- Formulario/script_F.js
+++ Formulario/script_F.js
@@ -1,3 +1,14 @@
-document.querySelector('form').addEventListener('submit', function() {
-    this.classList.add('enviando'); // Agrega una clase para animaciones
+// document.querySelector('form').addEventListener('submit', function() {
+//     this.classList.add('enviando'); // Agrega una clase para animaciones
+// });
+
+const checkbox = document.getElementById('validationCheckbox');
+const cap = document.getElementById('captcha');
+
+checkbox.addEventListener('change', function() {
+    if (checkbox.checked) {
+        cap.style.display = 'block';
+    }else{
+        cap.style.display = 'none';
+    }
 });
