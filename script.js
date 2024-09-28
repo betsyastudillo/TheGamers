fdc205982f29bf2aa1e55f148e19d9b70a9bd3f8
lovecraft-PGM <devlovecraft@gmail.com>
2024-09-27 21:28:37 -0500

    Finish work :)

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
 const carousel = document.querySelector('.carousel-inner');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-const items = document.querySelectorAll('.carousel-item');
+const images = document.querySelectorAll('.carousel-inner img');
 
 let currentIndex = 0;
-const itemWidth = items[0].clientWidth;
 
-nextBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % items.length;
-  carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
-
-  if (currentIndex === items.length - 1) {
-    currentIndex = 0; 
-    carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
-  }
-});
-
-prevBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + items.length) % items.length;
-  carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
-
-  if (currentIndex === 0) {
-    currentIndex = items.length - 1; 
-    carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
-  }
-});
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % images.length; 
+  carousel.style.transform = `translateX(-${currentIndex * 17}%)`;
+}, 2000); 
